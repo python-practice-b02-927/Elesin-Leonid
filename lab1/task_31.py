21dc55452234cb43cc49fe215934c609acdaa5b0
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-09-16 21:56:18 +0500

    Update task_31.py

--- lab1/task_31.py
+++ lab1/task_31.py
@@ -2,31 +2,26 @@
 
 from pyrob.api import *
 
-    
 
-
-@task(delay=0.05)
+@task(delay=0.001)
 def task_8_30():
-    i=0
-    b=0
-    while 1:
-        while not wall_is_on_the_right():
-            while not wall_is_beneath():
-                move_down()
-                i=0
-            move_right()
-        i=i+1
-        while not wall_is_on_the_left():
-            while not wall_is_beneath():
-                move_down()
-                i=0
-            move_left()
-        i=i+1
-        if i>1:
-            break
+        i=0
+        while 1:
+                while not wall_is_on_the_right():
+                        move_right()
+                i+=1
+                while not wall_is_on_the_left():
+                        move_left()
+                        if not wall_is_beneath():
+                            move_down()
+                            i=0
+                if i==1:
+                    break
+                
+                            
+                
 
 
-        
 
 if __name__ == '__main__':
     run_tasks()
