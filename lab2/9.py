4a24cbab4f9da52a2ffc14be002cf87d4a195bc2
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-09-16 22:21:49 +0500

    Update 9.py

--- lab2/9.py
+++ lab2/9.py
@@ -2,20 +2,20 @@ import turtle
 import math
 
 
-x=5
-i=3
-R=30
-a=R
-k=0
-turtle.shape('turtle')
-while i<13:
-    turtle.left(180-(180*(i-2)/i)*0.5)
-    while k<i:
+def mnogoug():
+    for k in range(i):
         c=a*2*math.sin(((2*math.pi)/(i*2)))
         turtle.forward(c)
         turtle.left(180-(180*(i-2)/i))
-        k+=1
-    k=0
+
+        
+i=3
+R=30
+a=R
+turtle.shape('turtle')
+for i in range (3, 13):
+    turtle.left(180-(180*(i-2)/i)*0.5)
+    mnogoug()
     turtle.right(180-(180*(i-2)/i)*0.5)
     turtle.penup()
     turtle.forward(R)
@@ -23,4 +23,4 @@ while i<13:
     i+=1
     turtle.pendown()
     
-    
+       
