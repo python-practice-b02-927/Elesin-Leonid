80e9bf57747e31dbaf78548bf63071ab70fd179a
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-09-16 18:04:57 +0500

    Update task_21.py

--- lab1/task_21.py
+++ lab1/task_21.py
@@ -5,20 +5,18 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_4_11():
-    i=0
-    b=0
-    z=0
-    while z<=12:
+
+    for z in range(13):
         
-        while i<13-z:
+        for i in range (13-z):
             move_down()
             move_right()
             fill_cell()
-            i=i+1
-        while b<12-z:
+        
+        for b in range (12-z):
             move_left()
             move_up()
-            b=b+1
+            
         move_left()
         i=0
         b=0
