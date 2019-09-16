313d53914b503ba4d0f41d004a664a2d4c82fc5e
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-09-16 18:10:01 +0500

    Update task_22.py

--- lab1/task_22.py
+++ lab1/task_22.py
@@ -2,27 +2,28 @@
 
 from pyrob.api import *
 
-
-@task
-def task_5_10():
-    fill_cell()
-    b=1
-    k=0
+def Right_Left():
     while not wall_is_on_the_right():
         move_right()
         fill_cell()
     while not wall_is_on_the_left():
         move_left()
     
+
+
+@task
+def task_5_10():
+    fill_cell()
+    b=1
+    k=0
+
+    Right_Left()
+    
     while not wall_is_beneath():
     
         move_down()
         fill_cell()
-        while not wall_is_on_the_right():
-            move_right()
-            fill_cell()
-        while not wall_is_on_the_left():
-            move_left()
+        Right_Left()
         if wall_is_beneath():
             break
        
