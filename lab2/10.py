d0bcbfbb07a592d092a0fd68521a0cd604b8eaa8
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-09-16 22:07:53 +0500

    Update 10.py

--- lab2/10.py
+++ lab2/10.py
@@ -1,23 +1,4 @@
 import turtle
-
-turtle.shape('turtle')
-i=0
-k=0
-turtle.speed (1)
-while k<3:
-    while i<1000:
-        turtle.forward(1)
-        turtle.left (360/1000)
-        i+=1
-    i=0
-    while i<1000:
-        turtle.forward(1)
-        turtle.right (360/1000)
-        i+=1
-    i=0
-    turtle.left (60)
-    k=k+1
-
-
-    
-input() 
+for i in range(6):
+    turtle.circle(100)
+    turtle.left(60)
