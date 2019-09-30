163d52e0890e6f1bcdcaf0e808150721420b84ca
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-10-01 01:06:22 +0500

    Added background

--- lab3/3.py
+++ lab3/3.py
@@ -1,31 +1,38 @@
 import graphics as gr
 
 
-def draw_body_and_tail(win):
+def draw_(win):
     """draw body and tail"""
     
 
 
-def draw_head(win):
+def draw_mountains(win):
     pass
 
 
-def draw_legs(win):
+def draw_birds(win):
     pass
 
 
-def draw_window(win):
+def draw_sun(win):
     pass
 
 
 def draw_background(win):
     """Draws wall and floor"""
-    wall = gr.Rectangle(gr.Point(0, 0), gr.Point(600, 400))
-    wall.setFill('orange4')
-    floor = gr.Rectangle(gr.Point(0, 400), gr.Point(600, 900))
-    floor.setFill('goldenrod4')
-    wall.draw(win)
-    floor.draw(win)
+    sky_above = gr.Rectangle(gr.Point(0, 0), gr.Point(900, 180))
+    sky_above.setFill('burlywood1')
+    sky_beneath = gr.Rectangle(gr.Point(0, 180), gr.Point(900, 360))
+    sky_beneath.setFill('pink')
+    sand = gr.Rectangle(gr.Point(0, 360), gr.Point(900, 540))
+    sand.setFill ('LightGoldenrod1')
+    abyss= gr.Rectangle(gr.Point(0, 540), gr.Point(900, 900))
+    abyss.setFill ('thistle')
+    sky_above.draw(win)
+    sky_beneath.draw(win)
+    sand.draw(win)
+    abyss.draw(win)
+    
 
     
 def main(win):
@@ -33,7 +40,7 @@ def main(win):
     draw_background(win)
 
     
-w = gr.GraphWin('pic8_1', 600, 900)
+w = gr.GraphWin('pic8_1', 900, 900)
 
 main(w)
 w.getMouse()
