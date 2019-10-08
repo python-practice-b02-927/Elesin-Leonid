efb05d71867889fb7b6e905d1715bcd1f23ed00a
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-10-08 23:05:02 +0500

    Add force, speed, movement and acceleration

--- lab3/2.animation.py
+++ lab3/2.animation.py
@@ -43,8 +43,74 @@ venus = gr.Circle(gr.Point(xv,yv), 10)
 venus.setFill('yellow')
 venus.draw(window)
 
+for i in range (10000000000000):
+    r_earth_mars=((xe-xm)**2+(ye-ym)**2)**0.5
+    r_earth_venus=((xe-xv)**2+(ye-yv)**2)**0.5
+    r_mars_venus=((xm-xv)**2+(ym-yv)**2)**0.5
 
+    f_earth_venus_x= (g*mass_of_earth*mass_of_venus/r_earth_venus**3)*(xv-xe)
+    f_earth_mars_x=(g*mass_of_earth*mass_of_mars/r_earth_mars**3)*(xm-xe)
+    f_earth_venus_y= (g*mass_of_earth*mass_of_venus/r_earth_venus**3)*(yv-ye)
+    f_earth_mars_y=(g*mass_of_earth*mass_of_mars/r_earth_mars**3)*(ym-ye)
+
+
+    xe=xe+uex*t
+    ye=ye+uey*t
+
+    dxe=uex*t
+    dye=uey*t
+
+    earth.move(dxe, dye)
+
+    aex=(f_earth_venus_x+f_earth_mars_x)/mass_of_earth
+    aey=(f_earth_venus_y+f_earth_mars_y)/mass_of_earth
+
+    uex=uex+aex*t
+    uey=uey+aey*t
+
+
+
+    f_mars_venus_x= (g*mass_of_mars*mass_of_venus/r_mars_venus**3)*(xv-xm)
+    f_mars_earth_x=-1*f_earth_mars_x
+    f_mars_venus_y= (g*mass_of_mars*mass_of_venus/r_mars_venus**3)*(yv-ym)
+    f_mars_earth_y=-1*f_earth_mars_y
     
+    xm=xm+umx*t
+    ym=ym+umy*t
+    
+    dxm=umx*t
+    dym=umy*t
+    
+    mars.move(dxm, dym)
+
+    amx=(f_mars_venus_x+f_mars_earth_x)/mass_of_mars
+    amy=(f_mars_venus_y+f_mars_earth_y)/mass_of_mars
+
+    umx=umx+amx*t
+    umy=umy+amy*t
+    
+
+    f_venus_mars_x=-1*f_mars_venus_x
+    f_venus_earth_x=-1*f_earth_venus_x
+    f_venus_mars_y=-1*f_mars_venus_x
+    f_venus_earth_y=-1*f_earth_venus_y
+
+
+
+    xv=xv+uvx*t
+    yv=yv+uvy*t
+
+    dxv=uvx*t
+    dyv=uvy*t
+
+    venus.move(dxv, dyv)
+    
+    avx=(f_venus_mars_x+f_venus_earth_x)/mass_of_venus
+    avy=(f_venus_mars_y+f_venus_earth_y)/mass_of_venus
+
+    uvx=uvx+avx*t
+    uvy=uvy+avy*t
+
     
 
 
