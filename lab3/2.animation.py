4d0261d18c90ea5bbc9b6146b7f63d9b70f96b46
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-10-08 23:01:05 +0500

    Add bodies and space

--- lab3/2.animation.py
+++ lab3/2.animation.py
@@ -1,34 +1,51 @@
 import graphics as gr
 window = gr.GraphWin("3 bodies problem", 1000, 1000)
+space=gr.Rectangle(gr.Point(0,0), gr.Point(1000,1000))
+space.setFill('black')
+space.draw(window)
 
 
 """Determine fundamental constants"""
-mass_of_earth=5.97E24
-mass_of_venus=4.87E24
-mass_of_mars=6.39E23
-g=6.67E-11
-t=0.0001
-
-"""Determine nachaline uslovia"""
+mass_of_earth=100
+mass_of_venus=20000
+mass_of_mars=100
+g=0.05
+t=0.01
 
 
+"""Determine initial conditions"""
 xe=500
-ye=500
-uex=10.8
+ye=700
+uex=1
 uey=0
-
-
 xv=500
-yv=538
-uvx=12.6
+yv=300
+uvx=0
 uvy=0
-
-
-
-xm=444
+xm=500
 ym=500
-umx=8.7
+umx=2
 umy=0
+
+
+"""Draw bodies"""
+earth = gr.Circle(gr.Point(xe,ye), 10)
+earth.setFill('blue')
+earth.draw(window)
+
+
+mars = gr.Circle(gr.Point(xm,ym), 10)
+mars.setFill('red')
+mars.draw(window)
+
+ 
+venus = gr.Circle(gr.Point(xv,yv), 10)
+venus.setFill('yellow')
+venus.draw(window)
+
+
+    
+    
 
 
 
