e34f8799c7b38cf7f91635fb0cb267f92f133f3a
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-10-08 20:16:43 +0500

    Add fundamental constants and initial conditions

---
+++ lab3/2.animation.py
@@ -0,0 +1,36 @@
+import graphics as gr
+window = gr.GraphWin("3 bodies problem", 1000, 1000)
+
+
+"""Determine fundamental constants"""
+mass_of_earth=5.97E24
+mass_of_venus=4.87E24
+mass_of_mars=6.39E23
+g=6.67E-11
+t=0.0001
+
+"""Determine nachaline uslovia"""
+
+
+xe=500
+ye=500
+uex=10.8
+uey=0
+
+
+xv=500
+yv=538
+uvx=12.6
+uvy=0
+
+
+
+xm=444
+ym=500
+umx=8.7
+umy=0
+
+
+
+
+
