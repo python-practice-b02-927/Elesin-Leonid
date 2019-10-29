f858710285fb8dec9b2c0aa7d8c931e60be6e2d6
LeoYe-st <55136387+LeoYe-st@users.noreply.github.com>
2019-10-29 11:37:58 +0300

    Added 2nd ball and they are moving

---
+++ lab6/gun.py
@@ -0,0 +1,188 @@
+from random import randrange as rnd, choice
+import tkinter as tk
+import math
+import time
+
+
+root = tk.Tk()
+fr = tk.Frame(root)
+root.geometry('800x600')
+canv = tk.Canvas(root, bg='white')
+canv.pack(fill=tk.BOTH, expand=1)
+
+
+class ball():
+    def __init__(self, x=40, y=450):
+        self.x = x
+        self.y = y
+        self.r = 10
+        self.vx = 0
+        self.vy = 0
+        self.color = choice(['blue', 'green', 'red', 'yellow'])
+        self.id = canv.create_oval(
+                self.x - self.r,
+                self.y - self.r,
+                self.x + self.r,
+                self.y + self.r,
+                fill=self.color
+        )
+        self.live = 30
+
+    def set_coords(self):
+        canv.coords(
+                self.id,
+                self.x - self.r,
+                self.y - self.r,
+                self.x + self.r,
+                self.y + self.r
+        )
+
+    def move(self):
+        self.x += self.vx
+        self.y -= self.vy
+        self.vy += -1.2
+        if self.x>(800-self.r):
+            self.vx=-self.vx
+        if self.y>(600-self.r):
+            self.vy=-self.vy
+        if self.x<self.r:
+            self.vx=-self.vx
+        if self.y<self.r:
+            self.vy=-self.vy
+        self.set_coords()
+        
+
+    def hittest(self, obj):
+
+        if abs(obj.x-self.x)<=(self.r+obj.r) and abs(obj.y-self.y)<=(self.r+obj.r):
+            return True
+        else:
+           return False
+
+        
+
+
+class gun():
+    def __init__(self):
+        self.f2_power = 10
+        self.f2_on = 0
+        self.an = 1
+        self.id = canv.create_line(20,450,50,420,width=7)
+    
+    def fire2_start(self, event):
+        self.f2_on = 1
+
+    def fire2_end(self, event):
+        
+        global balls, bullet
+        bullet += 1
+        new_ball = ball()
+        new_ball.r += 5
+        self.an = math.atan((event.y-new_ball.y) / (event.x-new_ball.x))
+        new_ball.vx = self.f2_power * math.cos(self.an)
+        new_ball.vy = - self.f2_power * math.sin(self.an)
+        balls += [new_ball]
+        self.f2_on = 0
+        self.f2_power = 10
+
+    def targetting(self, event=0):
+        
+    
+        if event:
+            self.an = math.atan((event.y-450) / (event.x-20))
+        if self.f2_on:
+            canv.itemconfig(self.id, fill='orange')
+        else:
+            canv.itemconfig(self.id, fill='black')
+        canv.coords(self.id, 20, 450,
+                    
+                    20 + max(self.f2_power, 20) * math.cos(self.an),
+                    450 + max(self.f2_power, 20) * math.sin(self.an)
+                    )
+
+    def power_up(self):
+        if self.f2_on:
+            if self.f2_power < 100:
+                self.f2_power += 1
+            canv.itemconfig(self.id, fill='orange')
+        else:
+            canv.itemconfig(self.id, fill='black')
+
+
+class target():
+    def __init__(self):
+        self.points=0
+        self.id=canv.create_oval(0,0,0,0)
+        self.id_points=canv.create_text(30,30,text=self.points,font='28')
+        self.new_target()
+        self.live=1
+
+
+    def new_target(self):
+        
+        x=self.x=rnd(600,780)
+        y=self.y=rnd(300,550)
+        r=self.r=rnd(2,50)
+        self.uy=rnd(-10,10)
+        color=self.color='red'
+        canv.coords(self.id,x-r,y-r,x+r,y+r)
+        canv.itemconfig(self.id,fill=color)
+
+    def move(self):
+
+        self.y+=self.uy
+        canv.coords(self.id, self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
+        if self.y>(600-self.r):
+            self.uy=-self.uy
+        if self.y<(self.r):
+            self.uy=-self.uy
+    def hit(self, points=1):
+        
+        canv.coords(self.id, -10, -10, -10, -10)
+        self.points += points
+        canv.itemconfig(self.id_points, text=self.points)
+
+    
+
+    
+
+
+t1 = target()
+t2=target ()
+screen1 = canv.create_text(400, 300, text='', font='28')
+g1 = gun()
+bullet = 0
+balls = []
+
+
+def new_game(event=''):
+    global gun, t1, screen1, balls, bullet
+    bullet = 0
+    balls = []
+    canv.bind('<Button-1>', g1.fire2_start)
+    canv.bind('<ButtonRelease-1>', g1.fire2_end)
+    canv.bind('<Motion>', g1.targetting)
+    z = 0.03
+    t1.live = 1
+    while t1.live or t2.live:
+        for i, b in enumerate(balls):
+            b.move()
+            if b.hittest(t1):
+                t1.live = 0
+                canv.coords(t1.id, -10, -10, -10, -10)
+            if b.hittest(t2):
+                t2.live = 0
+                canv.coords(t2.id, -10, -10, -10, -10)
+        if t1.live==1:
+            t1.move()
+        if t2.live == 1:
+            t2.move()
+        canv.update()
+        time.sleep(0.03)
+        g1.targetting()
+        g1.power_up()
+
+
+new_game()
+
+mainloop()
